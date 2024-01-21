50a1e615af68ba488cce05d05abc084521b4f053
LonliLucas <lukej.gilless@gmail.com>
2024-01-21 13:15:30 -0700

    Added some styles to App.css and basic rendering code to PorfolioHome.jsx

---
+++ src/components/PortfolioHome.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react';
+// import AboutMe from './AboutMe';
+// import ContactMe from './ContactMe';
+// import Portfolio from './Portfolio';
+// import Resume from './Resume';
+
+function PortfolioHome() {
+    return (
+        <header>
+            <h1>My Portfolio Placeholder</h1>
+        </header>
+    )
+}
+
+export default PortfolioHome;
