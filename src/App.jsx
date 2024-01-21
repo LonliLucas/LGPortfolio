b27153f09f2eaba6fa00d29356d67c3e9cd07cbd
LonliLucas <lukej.gilless@gmail.com>
2024-01-21 12:16:11 -0700

    Added some code to jsx files and added PortfolioHome component file'

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import PortfolioHome from './components/PortfolioHome.jsx';
+
+function App() {
+    return (
+        <div className="porfolio-app">
+            <PortfolioHome />
+        </div>
+    )
+}
+
+export default App;
