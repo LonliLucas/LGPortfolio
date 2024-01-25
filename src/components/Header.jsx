19ef58d7703fa471d79a2ab99de7e2d5d9f23777
LonliLucas <lukej.gilless@gmail.com>
2024-01-25 08:54:39 -0700

    Added header buttons to change pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 function Header() {
     return (
-        <header>
-                <h1 className="text-5xl">Lucas Gilles</h1>
+        <header className="flex ">
+                <h1 className="text-5xl p-5">Lucas Gilles</h1>
+                <ul className="ml-auto flex space-x-4 pr-5">
+                    <li><button className="about-me-button text-3xl mt-6 ml-4 mr-4">About Me</button></li>
+                    <li><button className="portfolio-button text-3xl mt-6 ml-4 mr-4">Portfolio</button></li>
+                    <li><button className="resume-button text-3xl mt-6 ml-4 mr-4">Resume</button></li>
+                    <li><button className="contactme-button text-3xl mt-6 ml-4 mr-4">Contact Me</button></li>
+                </ul>
          </header>
     )
 }
