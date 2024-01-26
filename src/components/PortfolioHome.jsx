8f0041bf6c7d57ad4a3cf1501633c2288492e943
LonliLucas <lukej.gilless@gmail.com>
2024-01-26 10:14:42 -0700

    Edited code to render separate pages when clicked

--- src/components/PortfolioHome.jsx
+++ src/components/PortfolioHome.jsx
@@ -1,16 +1,37 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import AboutMe from './AboutMe';
-// import ContactMe from './ContactMe';
-// import Portfolio from './Portfolio';
-// import Resume from './Resume';
+import AboutMe from './pages/AboutMe';
+// import ContactMe from './pages/ContactMe';
+// import Portfolio from './pages/Portfolio';
+// import Resume from './pages/Resume';
 
 function PortfolioHome() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    const renderPage = () => {
+        if (currentPage === 'AboutMe') {
+            return <AboutMe />
+        }
+        // if (currentPage === 'ContactMe') {
+        //     return <ContactMe />
+        // }
+        // if (currentPage === 'Portfolio') {
+        //     return <Portfolio />
+        // }
+        // if (currentPage === 'Resume') {
+        //     return <Resume />
+        // }
+    };
+
+    console.log(currentPage);
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
     return (
         <div className="flex flex-col min-h-screen justify-between">
-            <Header />
-            <AboutMe />
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+            <main className="mx-3">{renderPage()}</main>
             <Footer />
         </div>
     );
