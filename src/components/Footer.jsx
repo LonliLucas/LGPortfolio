36b21de98233b96ae0af901f8140e02f91ce0f64
Lucas Gilles <lucas.gilles@terralogic.com>
2024-01-30 11:51:23 -0700

    Added links to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 function Footer() {
     return (
         <footer className="text-lg">
-            <p className="p-8">Placeholder Footer Stuff</p>
+            <ul className="flex justify-between p-8">
+                <a href="https://github.com/LonliLucas" target="_blank" rel="noopener noreferrer">Github</a>
+                <a href="https://www.linkedin.com/in/lucas-gilles-a323761a7/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                <a href="https://www.instagram.com/lukej.gilless?igsh=bzNqemF1dXR6bnBw" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </ul>
         </footer>
     )
 }
